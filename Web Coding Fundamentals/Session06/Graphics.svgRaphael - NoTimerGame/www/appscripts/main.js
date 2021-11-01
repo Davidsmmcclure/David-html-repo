037fa27082c27b995e5a1f133ccc1cb2e3a25764
David McClure <david.mcclure@bazaarvoice.com>
2021-11-01 16:44:22 +0000

    session4&6
    session 4 and 6

---
+++ Web Coding Fundamentals/Session06/Graphics.svgRaphael - NoTimerGame/www/appscripts/main.js
@@ -0,0 +1,11 @@
+require(
+    [],
+    function () {
+            
+        console.log("yo, I'm alive!");
+
+        var paper = new Raphael(document.getElementById("mySVGCanvas"));
+
+ 
+    }
+);
